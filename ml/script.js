44b2a4c15b3af35a10418e8138a6b28d688fdd4b
tobiasvossen <tobias.vossen@study.hs-duesseldorf.de>
2020-05-27 23:06:37 +0200

    Exemplary commit

--- ml/script.js
+++ ml/script.js
@@ -1,4 +1,11 @@
 const video = document.getElementById('video');
+const config = {
+  audio: false,
+  video: {
+    facingMode: 'user'
+  }
+};
+
 var webcam;
 var running = true;
 
@@ -7,15 +14,15 @@ async function app() {
   let net = await mobilenet.load();
   document.getElementById('top').innerText = "MobileNet ready";
 
-  webcam = await tf.data.webcam(video);
+  webcam = await tf.data.webcam(video, config);
   while (running) {
     const img = await webcam.capture();
     document.getElementById('top').innerText = "Webcam open";
     const result = await net.classify(img);
 
     document.getElementById('bottom').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
+      Prediction: ${result[0].className}\n
+      Probability: ${result[0].probability}
     `;
 
     // Dispose the tensor to release the memory.
@@ -28,7 +35,7 @@ function stop_video() {
   running = false;
   webcam.stop();
   document.getElementById('top').innerText = "Webcam closed";
-  document.getElementById('bottom').innerText = "";
+  document.getElementById('bottom').innerText = "No prediction";
 }
 
 document.getElementById("stop").addEventListener("click", function(button) {
